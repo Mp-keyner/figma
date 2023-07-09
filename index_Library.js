5fc570c1f57a5a124a5803655d383c08721c8a5b
Mp-keyner <keyneroswaldo0@gmail.com>
2023-07-09 14:12:39 -0500

    9 de julio 2:12

--- index_Library.js
+++ index_Library.js
@@ -117,3 +117,55 @@ img6.forEach((img6) => {
   </g>
 </svg>`;
 });
+
+const img7 = document.querySelectorAll(".img7");
+
+img7.forEach((img7) => {
+  img7.innerHTML = `<svg class="svg-cutout" width="650" height="650" viewBox="0 0 650 650" fill="none" xmlns="http://www.w3.org/2000/svg" style="stroke: black; stroke-width: 4px; width: 100%; height: 100%;" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <pattern id="90bbe729-77e1-4102-8a5f-533992e3c942" patternUnits="objectBoundingBox" width="1" height="1">
+      <image href="//store.figma.com/cdn/shop/files/figma-store_homepage-hero_07_1000x.jpg?v=1678222214" x="0" y="0" width="100%" height="100%" class="pattern-image" preserveAspectRatio="xMidYMid slice"></image>
+    </pattern>
+  </defs>
+  <rect x="2" y="2" width="646" height="646" rx="103" fill="url(#90bbe729-77e1-4102-8a5f-533992e3c942)"></rect>
+</svg>`;
+});
+
+const img8 = document.querySelectorAll(".img8");
+
+img8.forEach((img8) => {
+  img8.innerHTML = `<svg class="svg-cutout" width="650" height="650" viewBox="0 0 650 650" fill="none" xmlns="http://www.w3.org/2000/svg" style="stroke: black; stroke-width: 4px; width: 100%; height: 100%;" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <pattern id="94e2a6be-8fcd-40c5-b48f-b52866f4b443" patternUnits="objectBoundingBox" width="1" height="1">
+      <image href="//store.figma.com/cdn/shop/files/figma-store_homepage-hero_08_1000x.jpg?v=1678222214" x="0" y="0" width="100%" height="100%" class="pattern-image" preserveAspectRatio="xMidYMid slice"></image>
+    </pattern>
+  </defs>
+  <path d="M647.629 324.502L649.477 323.737L647.629 324.502L553.485 97.2187C553.353 96.9001 553.1 96.6469 552.781 96.5148L325.498 2.37091C325.179 2.23889 324.821 2.23895 324.502 2.37091L323.737 0.523193L324.502 2.37097L97.2187 96.515C96.9 96.6469 96.6469 96.9001 96.5149 97.2188L2.37092 324.502C2.23893 324.821 2.23893 325.179 2.37093 325.498L96.5149 552.781C96.6469 553.1 96.9 553.353 97.2187 553.485L324.502 647.629C324.821 647.761 325.179 647.761 325.498 647.629L552.781 553.485C553.1 553.353 553.353 553.1 553.485 552.781L647.629 325.498C647.761 325.179 647.761 324.821 647.629 324.502Z" fill="url(#94e2a6be-8fcd-40c5-b48f-b52866f4b443)"></path>
+</svg>`;
+});
+
+const img9 = document.querySelectorAll(".img9");
+
+img9.forEach((img9) => {
+  img9.innerHTML = `<svg class="svg-cutout" width="650" height="650" viewBox="0 0 650 650" fill="none" xmlns="http://www.w3.org/2000/svg" style="stroke: black; stroke-width: 4px; width: 100%; height: 100%;" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <pattern id="d4d5c9a0-d644-47ad-bcd6-c6bd7f56cb16" patternUnits="objectBoundingBox" width="1" height="1">
+      <image href="//store.figma.com/cdn/shop/files/figma-store_homepage-hero_09_1000x.jpg?v=1678222214" x="0" y="0" width="100%" height="100%" class="pattern-image" preserveAspectRatio="xMidYMid slice"></image>
+    </pattern>
+  </defs>
+  <path d="M648 325C648 503.388 503.388 648 325 648C146.612 648 2 503.388 2 325C2 146.612 146.612 2 325 2C503.388 2 648 146.612 648 325Z" fill="url(#d4d5c9a0-d644-47ad-bcd6-c6bd7f56cb16)"></path>
+</svg>`;
+});
+
+const img10 = document.querySelectorAll(".img10");
+
+img10.forEach((img10) => {
+  img10.innerHTML = `<svg class="svg-cutout" width="658" height="658" viewBox="-2 -2 654 654" fill="none" xmlns="http://www.w3.org/2000/svg" style="stroke: black; stroke-width: 4px; width: 100%; height: 100%;"  xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <pattern id="d9dcd03f-4f85-4f3a-aa86-a68d7215ea0b" patternUnits="objectBoundingBox" width="1" height="1">
+      <image href="//store.figma.com/cdn/shop/files/figma-store_homepage-hero_10_1000x.jpg?v=1678222214" x="0" y="0" width="100%" height="100%" class="pattern-image" preserveAspectRatio="xMidYMid slice"></image>
+    </pattern>
+  </defs>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M650 0H0V125.496C0 231.179 81.9707 317.718 185.802 325C81.9707 332.282 0 418.821 0 524.504V650H650V524.504C650 418.82 568.029 332.282 464.198 325C568.029 317.718 650 231.179 650 125.496V0Z" fill="url(#d9dcd03f-4f85-4f3a-aa86-a68d7215ea0b)"></path>
+</svg>`;
+});
